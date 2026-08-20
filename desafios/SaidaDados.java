b60ca85525d0199ccdf6be2336f6b92cddbb5cde
Alailson <alailson.gomes.br@gmail.com>
2026-08-20 10:11:09 -0300

    SaidaDados
    Exercicios saida de dados. (Imprimir na tela)

---
+++ desafios/SaidaDados.java
@@ -0,0 +1,29 @@
+package desafios;
+
+public class SaidaDados {
+    public static void main(String[] args){
+
+        //nivel facil
+        System.out.println("Hello World!");
+        System.out.println("Estou aprendendo Java!\n");
+
+
+        //nivel medio
+        System.out.println("Nome: Alailson");
+        System.out.println("Curso: Ciência da Computação");
+        System.out.println("Linguagem: Java\n");
+
+
+        //nivel dificil
+        String linha = "========================================";
+        System.out.println(linha);
+        System.out.println("DADOS PESSOAIS");
+        System.out.println(linha);
+        System.out.println("Nome: Alailson ");
+        System.out.println("Curso: Ciência da Computação");
+        System.out.println("Linguagem: Java ");
+        System.out.println(linha+"\n");
+
+    }
+    
+}
