1e2a42e83f539c8ba531bbb3295e0359f5fb2578
Alailson <alailson.gomes.br@gmail.com>
2026-08-24 13:22:19 -0300

    EstruturasRepetição
    exercicios de estruturas de repetição

---
+++ desafios/EstruturaRepeticao.java
@@ -0,0 +1,45 @@
+package desafios;
+import java.util.Scanner;
+
+public class EstruturaRepeticao {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Nivel Facil
+        for(int i =1;i<=10;i++){
+            System.out.printf("Número: %d \n",i);
+        }
+
+
+        //Nivel Medio
+        System.out.println("Somar todos os numero até: ");
+        int n = scanner.nextInt();
+        int i = 0;
+        int soma = 0;
+        
+        while(i<n){
+            i = i+1;
+            soma +=i;
+        }
+
+        System.out.println(soma);
+
+        //Nivel Dificil 
+        String senha = "java123";
+        String tentativa;
+
+        
+        do{
+            System.out.print("Digite a senha: ");
+            tentativa = scanner.next();
+
+            if (tentativa.equals(senha)) {
+                System.out.println("Acesso concedido");
+            }else {
+                System.out.println("Acesso negado, tente novamente");
+            }
+
+        }while(!tentativa.equals(senha));
+    }
+}
